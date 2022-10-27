ab6af51692f5827fa1359676aad6e5c012a82e82
EC2 Default User <ec2-user@ip-172-31-2-105.ap-northeast-1.compute.internal>
2022-10-27 08:21:18 +0000

    [Start]13_rails_js_jquery

---
+++ app/javascript/script.js
@@ -0,0 +1,10 @@
+let title = "javascriptが使えました";
+alert(title);
+
+/*global $*/
+
+$(document).ready(function () {
+  $('.jquery').on('click', function(){
+    $(this).css('color','red');
+  });
+});
